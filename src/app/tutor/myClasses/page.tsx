e8f0b7c01d2b7388dd48121519e45da774999990
Stephan-Thomas <xevermontes8@gmail.com>
2025-10-16 16:20:20 +0100

    built tutor dashboard

---
+++ src/app/tutor/myClasses/page.tsx
@@ -0,0 +1,163 @@
+"use client";
+import React from "react";
+import Sidebar from "@/components/shared/Sidebar";
+import Navbar from "@/components/shared/Navbar";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { VscSettings } from "react-icons/vsc";
+import Link from "next/link";
+
+type TabType = "tab1" | "tab2" | "tab3";
+type Student = {
+  id: string;
+  name: string;
+  email: string;
+  grade: string;
+  status: string;
+};
+
+const page = () => {
+  const [activeTab, setActiveTab] = useState<TabType>("tab1");
+
+  const tabClasses = (tab: TabType) =>
+    `px-4 py-2 transition-colors ${
+      activeTab === tab
+        ? "text-blue-500 border-b border-b-blue-500"
+        : "text-gray-800 hover:text-blue-400"
+    }`;
+
+  const pathName = usePathname();
+
+  return (
+    <div className="flex gap-9">
+      <Sidebar />
+      <div className="flex-1">
+        <Navbar isTutor={true} />
+        <main>
+          <div className="w-full mt-8">
+            {/* Tabs */}
+            <div className="flex justify-between border-b border-gray-300">
+              <span className="flex space-x-2">
+                <Link
+                  href={"/tutor/students"}
+                  className={
+                    pathName === "/tutor/students"
+                      ? "text-[#029BD9] border-b border-b-[#029BD9]"
+                      : "text-[#666979]"
+                  }
+                >
+                  Students
+                </Link>
+                <Link href={"/tutor/assesment"}>Assesments</Link>
+                <Link href={"/tutor/leaderboard"}>Leaderboard</Link>
+              </span>
+
+              {activeTab === "tab1" && (
+                <span className="flex">
+                  <p className="text-gray-500">class ||</p>
+                  <p>React Fundamentals</p>
+                </span>
+              )}
+              {activeTab === "tab2" && (
+                <span className="flex gap-8">
+                  <button className="flex justify-center items-center gap-2 cursor-pointer mb-2">
+                    <VscSettings />
+                    Filter
+                  </button>
+                  <button className="bg-[#029BD9] text-white py-2 px-4 rounded-3xl cursor-pointer mb-2">
+                    Create New Assesment
+                  </button>
+                </span>
+              )}
+            </div>
+
+            {/* Tab Content */}
+            <div className="">
+              {activeTab === "tab1" && <div>some text here</div>}
+              {activeTab === "tab2" && (
+                <section>this is content for tab 2</section>
+              )}
+              {activeTab === "tab3" && (
+                <section>This is content for Tab 3.</section>
+              )}
+            </div>
+
+            <section>
+              <span className="flex flex-col flex-1/5 gap-6">
+                <span className="w-[10%] flex flex-col gap-3">
+                  <h2 className="text-[#666979] text-lg">My Classes</h2>
+                  <hr />
+                </span>
+                <div className="h-52 bg-[#F4F4F4] w-full rounded-3xl p-6 flex flex-col justify-between">
+                  <div className="flex justify-between">
+                    <h2 className="text-[#303240] font-semibold text-xl">
+                      React fundamentals
+                    </h2>
+                    <p className="bg-[#CEF6DF] border-[#CBEED9] text-[#059669] text-xs px-4 py-2 rounded-2xl">
+                      1 new submission
+                    </p>
+                  </div>
+                  <ul>
+                    <li className="text-[#666979] text-sm list-disc ml-8">
+                      15 students
+                    </li>
+                    <li className="text-[#666979] text-sm list-disc ml-8">
+                      3 assesments
+                    </li>
+                  </ul>
+                  <button className="cursor-pointer w-36 bg-[#1E1F27] text-white rounded-3xl py-3 px-6">
+                    View Class
+                  </button>
+                </div>
+                <div className="h-52 bg-[#F4F4F4] w-full rounded-3xl p-6 flex flex-col justify-between">
+                  <div className="flex justify-between">
+                    <h2 className="text-[#303240] font-semibold text-xl">
+                      React fundamentals
+                    </h2>
+                    <p className="bg-[#CEF6DF] border-[#CBEED9] text-[#059669] text-xs px-4 py-2 rounded-2xl">
+                      1 new submission
+                    </p>
+                  </div>
+                  <ul>
+                    <li className="text-[#666979] text-sm list-disc ml-8">
+                      15 students
+                    </li>
+                    <li className="text-[#666979] text-sm list-disc ml-8">
+                      3 assesments
+                    </li>
+                  </ul>
+                  <button className="cursor-pointer w-36 bg-[#1E1F27] text-white rounded-3xl py-3 px-6">
+                    View Class
+                  </button>
+                </div>
+                <div className="h-52 bg-[#F4F4F4] w-full rounded-3xl p-6 flex flex-col justify-between">
+                  <div className="flex justify-between">
+                    <h2 className="text-[#303240] font-semibold text-xl">
+                      React fundamentals
+                    </h2>
+                    <p className="bg-[#CEF6DF] border-[#CBEED9] text-[#059669] text-xs px-4 py-2 rounded-2xl">
+                      1 new submission
+                    </p>
+                  </div>
+                  <ul>
+                    <li className="text-[#666979] text-sm list-disc ml-8">
+                      15 students
+                    </li>
+                    <li className="text-[#666979] text-sm list-disc ml-8">
+                      3 assesments
+                    </li>
+                  </ul>
+                  <button className="cursor-pointer w-36 bg-[#1E1F27] text-white rounded-3xl py-3 px-6">
+                    View Class
+                  </button>
+                </div>
+              </span>
+            </section>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/app/tutor/page.tsx
+++ src/app/tutor/page.tsx
@@ -1,17 +1,112 @@
 import React from "react";
 import Sidebar from "@/components/shared/Sidebar";
 import Navbar from "@/components/shared/Navbar";
+import { CiSliderHorizontal } from "react-icons/ci";
 
 const page = () => {
   return (
-    <div className="flex">
+    <div className="flex gap-9">
       <Sidebar />
-      <div className="flex-1 ml-55">
+      <div className="flex-1">
         <Navbar isTutor={true} />
-        <div className="p-6">
-          <h2 className="text-2xl font-bold mb-4">Tutor Dashboard</h2>
-          {/* Add tutor-specific content here */}
-        </div>
+        <main className="p-6 max-w-8xl mr-9">
+          <section>
+            <article className="flex gap-5 flex-col mb-20 max-w-8xl">
+              <div>
+                <h2 className="text-lg font-medium mb-4">Welcome, User</h2>
+                <p className="text-sm text-[#8E91A1]">
+                  Manage your students assesments and activities smoothly.
+                </p>
+              </div>
+              <span className="flex justify-between gap-4 max-w-8xl">
+                <div className="flex-1 bg-[#F4F4F4] h-[115px] rounded-3xl flex flex-col justify-center items-center p-6">
+                  <h2 className="text-[#303240] text-3xl font-bold">3</h2>
+                  <p className="text-[#666979] text-sm">Active Classes</p>
+                </div>
+                <div className="flex-1 bg-[#F4F4F4] h-[115px] rounded-3xl flex flex-col justify-center items-center p-6">
+                  <h2 className="text-[#303240] text-3xl font-bold">47</h2>
+                  <p className="text-[#666979] text-sm">Total Students</p>
+                </div>
+                <div className="flex-1 bg-[#F4F4F4] h-[115px] rounded-3xl flex flex-col justify-center items-center p-6">
+                  <h2 className="text-[#303240] text-3xl font-bold">12</h2>
+                  <p className="text-[#666979] text-sm">Active Assessments</p>
+                </div>
+                <div className="flex-1 bg-[#F4F4F4] h-[115px] rounded-3xl flex flex-col justify-center items-center p-6">
+                  <h2 className="text-[#303240] text-3xl font-bold">89</h2>
+                  <p className="text-[#666979] text-sm">Pending Grades</p>
+                </div>
+              </span>
+              <span className="flex justify-between items-center gap-2 max-w-8xl text-white">
+                <p className="text-black">Quick Action:</p>
+                <button className="cursor-pointer flex-1 h-16 rounded-4xl px-4 py-5 bg-[#029BD9]">
+                  Create new class
+                </button>
+                <button className="cursor-pointer flex-1 h-16 rounded-4xl px-4 py-5 bg-[#1E1F27]">
+                  Create new class
+                </button>
+                <button className="cursor-pointer flex-1 h-16 rounded-4xl px-4 py-5 bg-[#1E1F27]">
+                  Create new class
+                </button>
+              </span>
+            </article>
+            <article className="max-w-8xl flex gap-11">
+              <span className="flex flex-1 flex-col gap-3 h-fit rounded-b-3xl shadow-[0px_6px_12px_0px_#BDBDBD1A,0px_23px_23px_0px_#BDBDBD17,0px_142px_40px_0px_#BDBDBD00]">
+                <div className="bg-[#F4F4F4] flex justify-between items-center py-2 px-6 h-14 rounded-t-3xl">
+                  <h2>Recent Activity</h2>
+                  <CiSliderHorizontal size={25} className="cursor-pointer" />
+                </div>
+                <div className="py-4 flex flex-col gap-3 px-5 pb-5">
+                  <h3 className="text-base text-[#666979]">
+                    React Fundamentals
+                  </h3>
+                  <span className="flex justify-between">
+                    <p className="text-sm text-[#8E91A1]">
+                      from REACT 001 in "React project"
+                    </p>
+                    <p className="text-xs text-[#8E91A1]">2 minutes ago</p>
+                  </span>
+                  <hr />
+                </div>
+                <div className="py-4 flex flex-col gap-3 px-5 pb-5">
+                  <h3 className="text-base text-[#666979]">
+                    React Fundamentals
+                  </h3>
+                  <span className="flex justify-between">
+                    <p className="text-sm text-[#8E91A1]">
+                      from REACT 001 in "React project"
+                    </p>
+                    <p className="text-xs text-[#8E91A1]">2 minutes ago</p>
+                  </span>
+                  <hr />
+                </div>
+                <div className="py-4 flex flex-col gap-3 px-5 pb-5">
+                  <h3 className="text-base text-[#666979]">
+                    React Fundamentals
+                  </h3>
+                  <span className="flex justify-between">
+                    <p className="text-sm text-[#8E91A1]">
+                      from REACT 001 in "React project"
+                    </p>
+                    <p className="text-xs text-[#8E91A1]">2 minutes ago</p>
+                  </span>
+                  <hr />
+                </div>
+                <div className="py-4 flex flex-col gap-3 px-5 pb-5">
+                  <h3 className="text-base text-[#666979]">
+                    React Fundamentals
+                  </h3>
+                  <span className="flex justify-between">
+                    <p className="text-sm text-[#8E91A1]">
+                      from REACT 001 in "React project"
+                    </p>
+                    <p className="text-xs text-[#8E91A1]">2 minutes ago</p>
+                  </span>
+                  <hr />
+                </div>
+              </span>
+            </article>
+          </section>
+        </main>
       </div>
     </div>
   );
